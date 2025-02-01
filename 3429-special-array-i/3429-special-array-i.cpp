bd55e6af06e27ad155e0205659e3757267d17ace
Armaan <120023465+kakkararmaan22@users.noreply.github.com>
2025-02-01 22:29:34 +0530

    Time: 0 ms (100%), Space: 27.7 MB (61.71%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        bool even = 0;
+        even = (nums[0]%2 == 0)?1:0;
+        if(even) {
+            for(int i = 0; i<nums.size(); i++) {
+                if(i%2==0 && nums[i]%2!=0) return false;
+                if(i%2!=0 && nums[i]%2==0) return false;
+            }
+        }
+        else {
+            for(int i = 0; i<nums.size(); i++) {
+                if(i%2==0 && nums[i]%2==0) return false;
+                if(i%2!=0 && nums[i]%2!=0) return false;
+            }
+        }
+        return true;
+    }
+};
