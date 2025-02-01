bb100c070bacbd7e03182c575db7ea59a2bb1b22
Armaan <120023465+kakkararmaan22@users.noreply.github.com>
2025-02-01 22:31:19 +0530

    Time: 0 ms (100%), Space: 27.6 MB (61.71%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -5,14 +5,12 @@ public:
         even = (nums[0]%2 == 0)?1:0;
         if(even) {
             for(int i = 0; i<nums.size(); i++) {
-                if(i%2==0 && nums[i]%2!=0) return false;
-                if(i%2!=0 && nums[i]%2==0) return false;
+                if((i%2==0 && nums[i]%2!=0) || (i%2!=0 && nums[i]%2==0)) return false;
             }
         }
         else {
             for(int i = 0; i<nums.size(); i++) {
-                if(i%2==0 && nums[i]%2==0) return false;
-                if(i%2!=0 && nums[i]%2!=0) return false;
+                if((i%2==0 && nums[i]%2==0) || (i%2!=0 && nums[i]%2!=0)) return false;
             }
         }
         return true;
