5bc772b8532e652ed8471acbcff4c8af10fd2764
Armaan <120023465+kakkararmaan22@users.noreply.github.com>
2025-02-12 11:54:01 +0530

    Time: 19 ms (76.31%), Space: 63.9 MB (61.17%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int sumOfDigits(int num) {
         int sum = 0;
-        while (num > 0) {
+        while (num) {
             sum += num % 10;
             num /= 10;
         }
