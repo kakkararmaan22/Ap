582ac36027e6215a5d4c155d35b49b9131609800
Armaan <120023465+kakkararmaan22@users.noreply.github.com>
2025-02-15 14:45:58 +0530

    Time: 508 ms (15.87%), Space: 536.5 MB (9.32%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    bool check(int i, int currSum, string &s, int num,vector<vector<int>> &dp) {
-        if(i == s.length()) {
+    bool check(int i, int currSum, string &s, int num, vector<vector<int>> &dp) {
+        if (i == s.length()) {
             return currSum == num;
         }
-        if(currSum > num){
-            return 0;
+        if (currSum > num) {
+            return false;
         }
         if (dp[i][currSum] != -1) return dp[i][currSum];
 
-        for(int j = i; j <s.length(); j++) {
-            string sub = s.substr(i, j-i+1);
-            int val = stoi(sub);
-            if(check(j+1, currSum+val, s, num,dp)) return dp[i][currSum] = 1;
+        int val = 0; // Maintain the integer value while iterating
+        for (int j = i; j < s.length(); j++) {
+            val = val * 10 + (s[j] - '0'); // Convert substring to integer without using stoi()
+            if (val > num) break; // Prune unnecessary computations
+            if (check(j + 1, currSum + val, s, num, dp)) 
+                return dp[i][currSum] = 1;
         }
         return dp[i][currSum] = 0;
     }
+
     int punishmentNumber(int n) {
         int sum = 0;
-        for(int num = 1;num<=n; num++) {
-            int data = num*num;
+        for (int num = 1; num <= n; num++) {
+            int data = num * num;
             string s = to_string(data);
-            vector<vector<int>> dp(s.length(), vector<int>(num+1,-1));
-            if(check(0, 0, s, num, dp)) {
-                sum =sum + data;
+            vector<vector<int>> dp(s.length(), vector<int>(num + 1, -1)); // Properly initialize dp
+            if (check(0, 0, s, num, dp)) {
+                sum += data;
             }
         }
         return sum;
