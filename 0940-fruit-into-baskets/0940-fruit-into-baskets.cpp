b0c029c0a0f040a45ebe4980503e35bed4e01a0d
Armaan <120023465+kakkararmaan22@users.noreply.github.com>
2025-02-16 15:05:43 +0530

    Time: 43 ms (50.88%), Space: 75 MB (58.42%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -5,13 +5,13 @@ public:
         unordered_map<int,int> mp;
         int left =0, ans = 0;
         for(int right = 0; right<n; right++) {
+            mp[fruits[right]]++;
             while(mp.size()>2) {
                 mp[fruits[left]]--;
                 if(mp[fruits[left]] == 0) mp.erase(fruits[left]);  
                 left++;
             }
             
-            mp[fruits[right]]++;
             if(mp.size()<=2) {
                 ans = max(ans, right-left+1);
             }
