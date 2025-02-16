2ac4743d5530b5c2e9ea252938a7a62eccac943e
Armaan <120023465+kakkararmaan22@users.noreply.github.com>
2025-02-16 13:29:42 +0530

    Time: 0 ms (100%), Space: 59.3 MB (59.8%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -5,11 +5,11 @@ public:
         int left = 0, ans=0, zeros =0;
         for(int right = 0; right<n; right++) {
             if(nums[right] == 0) zeros++;
-            while(zeros>k) {
+            if(zeros>k) {
                 if(nums[left] == 0) zeros--;
                 left++;
             }
-            ans= max(ans, right-left+1);
+            if(zeros <= k) ans= max(ans, right-left+1);
         }
         return ans;
     }
